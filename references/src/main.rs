d91e7398558b5e3e63c28db6f3bc1abe1cb2b0c4
Szymon Papuga <papuga.szymon@gmail.com>
2024-05-28 21:44:53 +0200

    references

---
+++ references/src/main.rs
@@ -0,0 +1,59 @@
+fn main() {
+    let s1 = String::from("hello");
+
+    let len = calculate_length(&s1);
+
+    println!("The length of '{}' is {}.", s1, len);
+
+    let mut s = String::from("hello");
+
+    change(&mut s);
+
+    let mut s = String::from("hello");
+
+    let r1 = &mut s;
+    let r2 = &mut s;
+
+    println!("{}, {}", r1, r2);
+
+    let mut s = String::from("hello");
+
+    {
+        let r1 = &mut s;
+    }
+
+    let r2 = &mut s;
+
+    let mut s = String::from("hello");
+
+    let r1 = &s;
+    let r2 = &s;
+    let r3 = &mut s;
+
+    println!("{}, {}, and {}", r1, r2, r3);
+
+    let mut s = String::from("hello");
+
+    let r1 = &s;
+    let r2 = &s;
+    println!("{} and {}", r1, r2);
+
+    let r3 = &mut s;
+    println!("{}", r3);
+
+    let reference_to_nothing = dangle();
+}
+
+fn calculate_length(s: &String) -> usize {
+    s.len()
+}
+
+fn change(some_string: &mut String) {
+    some_string.push_str(", world");
+}
+
+fn dangle() -> &String {
+    let s = String::from("hello");
+
+    &s
+}
