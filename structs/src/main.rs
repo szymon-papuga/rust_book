9611db20553b6f7524516b7332d915d6819017f0
Szymon Papuga <papuga.szymon@gmail.com>
2024-06-04 22:35:11 +0200

    structs

---
+++ structs/src/main.rs
@@ -0,0 +1,35 @@
+fn main() {
+    let mut user1 = User {
+        active: true,
+        username: String::from("someusername123"),
+        email: String::from("someone@example.com"),
+        sign_in_count: 1,
+    };
+
+    user1.email = String::from("anotheremail@example.com");
+
+    let user2 = User {
+        email: String::from("another@example.com"),
+        ..user1
+    };
+
+    let subject = AlwaysEqual;
+}
+
+struct User {
+    active: bool,
+    username: String,
+    email: String,
+    sign_in_count: u64,
+}
+
+fn build_user(email: String, username: String) -> User {
+    User {
+        active: true,
+        username,
+        email,
+        sign_in_count: 1,
+    }
+}
+
+struct AlwaysEqual;
