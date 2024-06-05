946ba147ca2d3a12bae5c84a454ab6727618654c
Szymon Papuga <papuga.szymon@gmail.com>
2024-06-05 16:30:43 +0200

    associated functions

---
+++ structs/associated_functions/src/main.rs
@@ -0,0 +1,17 @@
+struct Rectangle {
+    width: u32,
+    height: u32,
+}
+
+impl Rectangle {
+    fn square(size: u32) -> Self {
+        Self {
+            width: size,
+            height: size,
+        }
+    }
+}
+
+fn main() {
+    println!("Hello, world!");
+}
